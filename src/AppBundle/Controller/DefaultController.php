5fc79c9384dccb52a15e4e413bee55f1f3dbac29
Danny Hannah <danny@convertrmedia.net>
2016-08-25 10:33:57 +0100

    Adding basic interfaces and hardcoded forms

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Link;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,10 +15,37 @@ class DefaultController extends Controller
     public function indexAction(Request $request)
     {
 
-
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+
+        ]);
+    }
+
+    /**
+     * @Route("/link/add", name="link.add")
+     */
+    public function linkAddAction(Request $request)
+    {
+
+        if ($request->isMethod('post')) {
+            $link = (new Link())
+                ->setName($request->request->get('name'))
+                ->setDestination($request->request->get('destination'))
+                ->setCreateTs(new \DateTime())
+            ;
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($link);
+            $em->flush();
+
+            $this->addFlash('success', 'Link added successfully');
+
+            return $this->redirectToRoute('homepage');
+        }
+
+        // replace this example code with whatever you need
+        return $this->render('forms/link.html.twig', [
+
         ]);
     }
 }
